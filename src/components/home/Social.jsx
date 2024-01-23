6f5a5248c60a1d4b98e1687760bc17e3eb7ecb1e
Ryan Gonz <wisnu.2722@students.amikom.ac.id>
2024-01-23 17:58:56 +0700

    initial commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/ryan_gonzzalezz/" target='_blank' rel="noreferrer" className="home__social-icon">
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/wisnu-aditya-gonzz/" target='_blank' rel="noreferrer" className="home__social-icon">
+                <i className="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/Wisnu2722" target='_blank' rel="noreferrer" className="home__social-icon">
+                <i className="uil uil-github"></i>
+            </a>
+
+        </div>
+    )
+}
+
+export default Social
